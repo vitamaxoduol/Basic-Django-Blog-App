13def4f3847fda1d7f953a64cba5a4d5eb35d288
vitamaxoduol <vitamaxoduo@gmail.com>
2023-11-26 13:57:03 +0300

    blog views, forms && models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    created_at =  models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     
     
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import BlogPost
+from .forms import BlogPostForm
+from django.urls import reverse_lazy
 
 
-def blog_list(request):
-    posts = BlogPost.objects.all().order_by('published_date')
-    return render(request, 'blog/blog_list.html', {'posts': posts})
+class BlogPostListView(ListView):
+    # posts = BlogPost.objects.all().order_by('published_date')
+    # return render(request, 'blog/blog_list.html', {'posts': posts})
+
+    model = BlogPost
+    template_name = 'blogpost_list.html'
+    
+    
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blogpost_detail.html'
+    
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'blogpost_create.html'
+    success_url = reverse_lazy('blogpost_list')
+    
+class BlogPostUpdateView(UpdateView):
+    model =  BlogPost
+    form_class = BlogPostForm
+    template_name = 'blogpost_update.html'
+    success_url = reverse_lazy('blogpost_list')
+    
+class BlogPostDeleteView(DeleteView):
+    models = BlogPost
+    template_name = "blogpost_delete.html"
+    success_url = reverse_lazy('blogpost_list')
+    
